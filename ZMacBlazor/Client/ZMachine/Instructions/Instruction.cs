117389710d9d7a015528f8d15254b27b9450776f
Scott Allen <scott@odetocode.com>
2019-11-27 15:24:32 -0500

    docs for instruction decoding

--- ZMacBlazor/Client/ZMachine/Instructions/Instruction.cs
+++ ZMacBlazor/Client/ZMachine/Instructions/Instruction.cs
@@ -4,6 +4,41 @@ using System.Collections.Generic;
 
 namespace ZMacBlazor.Client.ZMachine.Instructions
 {
+    // f - form
+    // o - opcode
+    // t - opcode type
+    // * - operand type
+    // 1,2,3,4 - operands
+    //
+    // Long 2OP
+    // f**ooooo 11111111 22222222
+    // 0xxxxxxx xxxxxxxx xxxxxxxx
+    //
+    // * - 0 small constant 1 is variable
+    //
+    // Short 0OP / 1OP
+    // ffttoooo 11111111
+    // 10xxxxxx xxxxxxxx
+    //
+    // if(tt == 11) 0OP else 1OP
+    //
+    // Extended EXT
+    // 0xBE     oooooooo ******** 11111111 22222222 33333333 44444444
+    // 10111110 xxxxxxxx xxxxxxxx xxxxxxxx xxxxxxxx xxxxxxxx xxxxxxxx
+    //
+    // Variable 2OP / VAR
+    // fftooooo ******** 11111111 22222222 33333333 44444444
+    // 11xxxxxx xxxxxxxx xxxxxxxx xxxxxxxx xxxxxxxx xxxxxxxx
+    // 
+    // if(t == 0) 2OP else VAROP 
+    //
+    // Double VAR (call_vs2 call vn_2)
+    // fftooooo ******** ******** 11111111 22222222 33333333 44444444 55555555 66666666 77777777 88888888
+    // 11xxxxxx xxxxxxxx xxxxxxxx xxxxxxxx xxxxxxxx xxxxxxxx xxxxxxxx xxxxxxxx xxxxxxxx xxxxxxxx xxxxxxxx 
+    // 
+    // if(t == 0) 2OP else VAROP 
+
+
     public class Instruction
     {
         public Instruction(ReadOnlySpan<byte> bytes)
@@ -11,6 +46,12 @@ namespace ZMacBlazor.Client.ZMachine.Instructions
             Form = DecodeForm(bytes[0]);
             OpCount = DecodeOpCount(bytes[0]);
             OpCode = DecodeOpCode(bytes);
+            OperandTypes = DecodeOperandTypes(bytes);
+        }
+
+        private ICollection<OperandType> DecodeOperandTypes(ReadOnlySpan<byte> bytes)
+        {
+            
         }
 
         private byte DecodeOpCode(ReadOnlySpan<byte> bytes)
